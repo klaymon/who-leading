4108f63542aac84a25fd2a28e225082d7c4813fa
Kyle Anne Laymon <kyle.laymon@gmail.com>
2020-11-06 00:51:11 -0800

    feat: show est votes remaining
    and some cleanup

--- pages/index.js
+++ pages/index.js
@@ -16,16 +16,17 @@ let reeeeee = {
 let intervalHold
 export default function Home() {
   useEffect(() => {
-    let restored = window.localStorage.getItem('wholeadingbackup')
-    if (restored) console.log({restored})
+    //cleanup
+      window.localStorage.removeItem('wholeadingbackup')
+    //cleanup
+    let restored = window.localStorage.getItem('whoisleadingbackup') || '{}'
+    restored = JSON.parse(restored)
     if (restored && restored?.timestamp) {
-      restored = JSON.parse(restored)
       setResults(restored.rows)
       timestamp = restored.timestamp
       setNewBatchTimestamp(restored.timestamp)
       setLastFetchTimestamp(restored.lastFetchTimestamp)
     } else {
-      console.log('fetchy')
       fetchy()
     }
     if (intervalHold) clearInterval(intervalHold)
@@ -36,41 +37,36 @@ export default function Home() {
   const [lastFetchTimestamp, setLastFetchTimestamp] = useState('')
   const fetchy = async () => {
     let data
-    setLastFetchTimestamp(Date.now())
     if (window && window.fetch) {
       data = await fetch('https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/votes-remaining-page/national/president.json')
       .then(response => response.json())
       .then(data => data || [])
     }
     if (data) {
-      let newTimestamp = new Date(data.meta.timestamp).getTime()
-      if (newTimestamp !== timestamp) {
-        console.log({newTimestamp})
-        timestamp = newTimestamp
-        let rows = []
-        STATE_INDEXES.forEach((index) => {
-          let race = data?.data?.races[index]
-          let record = {
-            timestamp,
-            state: race['state_id'],
-            candidates: race.candidates
-          }
-          if (reeeeee[index] !== race.candidates[0].votes) {
-            setNewBatchTimestamp(timestamp)
-            reeeeee[index] = race.candidates[0].votes
-            rows.push(record)
-          }
-        })
-        if (rows.length) {
-          if (results.timestamp) {
-            setPreviousData(results)
-          }
-          const backup = {lastFetchTimestamp, timestamp, rows}
-          const stringified = JSON.stringify(backup)
-          window.localStorage.setItem('wholeadingbackup', stringified)
-          setResults([...rows])
+      let rows = []
+      let hasUpdate = false
+      STATE_INDEXES.forEach((index) => {
+        let race = data?.data?.races[index]
+        if (!race) return
+        let record = {
+          candidates: race.candidates,
+          estRemainingVotes: race['tot_exp_vote'] - race.votes,
+          state: race['state_id']
         }
+        if (reeeeee[index] !== race.candidates[0].votes) {
+          hasUpdate = true
+          reeeeee[index] = race.candidates[0].votes
+          setNewBatchTimestamp(timestamp)
+        }
+        rows.push(record)
+      })
+      if (hasUpdate) {
+        const backup = {lastFetchTimestamp: Date.now(), timestamp, rows}
+        const stringified = JSON.stringify(backup)
+        window.localStorage.setItem('whoisleadingbackup', stringified)
+        setResults(rows)
       }
+      setLastFetchTimestamp(Date.now())
     }
   }
   return (
@@ -80,12 +76,12 @@ export default function Home() {
       </Head>
       {lastFetchTimestamp && <div><span>Last Checked: </span><span>{new Date(lastFetchTimestamp).toLocaleString()}</span></div>}
       {newBatchTimestamp && <div><span>Last New Batch: </span><span>{Math.round((Date.now() - newBatchTimestamp) / 60000)} minutes ago</span></div>}
-      <div className={styles.row} ><span>state</span> <span>candidate</span> <span>leads by</span></div>
+      <div className={styles.row} ><span>state</span> <span>candidate</span> <span>leads by</span> <span>est votes remaining</span></div>
       {results?.map((record) => {
         let leadingCandidate = record.candidates[0]
         let trailingCandidate = record.candidates[1]
         let color = leadingCandidate['last_name'] === 'Biden' ? 'blue' : 'red'
-        return <div className={`${styles.row} ${styles[color]}`} key={record.state}><span>{record.state}</span> <span>{leadingCandidate['last_name']}</span> <span>{leadingCandidate.votes - trailingCandidate.votes}</span></div>
+        return <div className={`${styles.row} ${styles[color]}`} key={record.state}><span>{record.state}</span> <span>{leadingCandidate['last_name']}</span> <span>{leadingCandidate.votes - trailingCandidate.votes}</span> <span>{record.estRemainingVotes}</span></div>
       })}
       <a href='https://twitter.com/explodedsoda' target='_blank' rel='noopener noreferrer' className={styles.teeny}>@</a>
     </div>
